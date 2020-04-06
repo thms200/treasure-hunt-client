118755d93fb7067a742db3094a94d6f81967249b
thms200 <thms0504@gmail.com>
2020-04-06 18:05:09 +0900

    upload server treasure information(images, category, country etc)

--- constants/message.js
+++ constants/message.js
@@ -1,9 +1,11 @@
 const message = {
   invalidLogin: 'Sorry, We can\'t login for unkown reasons. Try again.',
   deniedPermission: 'You have not enabled selected permissions.',
-  createPicture: 'Create your treasure picture! 👍',
+  createPicture: 'Create your treasure location picture! 👍',
   failPicture: 'Sorry. We can\'t make your treasure picture! Try again. 🥺',
   maxImg: 'Up to three images can be registered.',
+  failSave: 'Sorry. We can\'t save your treasure information! Try again.',
+  successSave: 'Success save your treasure. Thank you. Happy Hunting. 👍',
 };
 
 export default message;
--- screens/InputTreasureDetail.js
+++ screens/InputTreasureDetail.js
@@ -2,13 +2,17 @@ import React, { useState, useEffect } from 'react';
 import { View, Text, TouchableOpacity, Image } from 'react-native';
 import * as Permissions from 'expo-permissions';
 import * as ImagePicker from 'expo-image-picker';
+import * as SecureStore from 'expo-secure-store';
 import { COLOR } from '../constants';
 import message from '../constants/message';
+import getEnvVars from '../environment';
+const { API_URL } = getEnvVars();
 
 export default function InputTreasureDetail({ navigation, route }) {
   const [hasPermissionCamera, setHasPermissionCamara] = useState(false);
   const [hasPermissionGallery, setHasPermissionGallery] = useState(false);
   const [uriList, setUriList] = useState([]);
+  const { category } = route.params;
 
   useEffect(() => {
     (async() => {
@@ -38,7 +42,41 @@ export default function InputTreasureDetail({ navigation, route }) {
     }
   };
 
-  const { category } = route.params;
+  const onSaveTreasure = async() => {
+    try {
+      const formdata = new FormData();
+      uriList.forEach(uri => {
+        const name = uri.split('Camera/')[1] || uri.split('ImagePicker/')[1];
+        const locationPicture = { uri, name, type: 'multipart/form-data', };
+        formdata.append('img', locationPicture);
+      });
+      formdata.append('category', category);
+      formdata.append('country', 'france');
+      formdata.append('location', 37.505978);
+
+      const userToken = await SecureStore.getItemAsync('userToken');
+      return await fetch(`${API_URL}/api/treasures`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'multipart/form-data',
+          'x-access-token': `Bearer ${userToken}`,
+        },
+        body: formdata,
+      })
+        .then((res) => res.json())
+        .then((json) => {
+          if (json.result === 'ok') {
+            alert(message.successSave);
+            return navigation.navigate('GetTreasureList');
+          }
+          if (json.result === 'ng') return alert(message.failSave);
+        });
+    } catch(err) {
+      alert(message.failSave);
+      console.warn(err);
+    }
+  };
+
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>{category}</Text>
@@ -79,6 +117,12 @@ export default function InputTreasureDetail({ navigation, route }) {
           </View>
         );
       })}
+      <TouchableOpacity
+        style={{ margin: 10, padding: 10 }}
+        onPress={() => onSaveTreasure()}
+      >
+        <Text style={{ color: COLOR.BLUE }}>Complete</Text>
+      </TouchableOpacity>
     </View>
   );
 }
