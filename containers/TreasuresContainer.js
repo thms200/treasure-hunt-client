39afb5143a39db2f5a85d152488992aea2b2fa28
thms200 <thms0504@gmail.com>
2020-04-11 13:51:46 +0900

    Add 'isloaing' in Treasures Page

--- containers/TreasuresContainer.js
+++ containers/TreasuresContainer.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { StyleSheet, View, FlatList } from 'react-native';
 import TreasureList from '../components/TreasureList';
@@ -8,6 +8,7 @@ import { fetchTreasures, fetchSelectedTreasure } from '../utils/api';
 import { COLOR } from '../constants';
 
 export default function TreasuresContainer({ navigation }) {
+  const [isLoading, setIsLoading] = useState(true);
   const treasures = useSelector(state => state.treasures.treasures);
   const dispatch = useDispatch();
   const onClickCategory = async(category) => await fetchTreasures('all', category, dispatch, getTreasures);
@@ -15,33 +16,40 @@ export default function TreasuresContainer({ navigation }) {
   const onClickTreasure = async(id) => await fetchSelectedTreasure(id, dispatch, getSelectedTreasure);
 
   useEffect(() => {
-    fetchTreasures('all', 'all', dispatch, getTreasures);
+    (async() => {
+      await fetchTreasures('all', 'all', dispatch, getTreasures);
+      setIsLoading(false);
+    })();
   }, []);
 
-  return (
-    <View style={{ flex: 1 }}>
-      <Categorys onClickCategory={onClickCategory} />
-      <View style={styles.treasuresWrapper}>
-        <FlatList
-          data={treasures}
-          renderItem={({ item }) => {
-            return (
-              <TreasureList
-                name={item.name}
-                country={item.country}
-                expiration={item.expiration}
-                id={item.id}
-                navigation={navigation}
-                onClickCountry={onClickCountry}
-                onClickTreasure={onClickTreasure}
-              />
-            );
-          }}
-          keyExtractor={item => item.id}
-        />
+  if(isLoading) {
+    return <View />;
+  } else {
+    return (
+      <View style={{ flex: 1 }}>
+        <Categorys onClickCategory={onClickCategory} />
+        <View style={styles.treasuresWrapper}>
+          <FlatList
+            data={treasures}
+            renderItem={({ item }) => {
+              return (
+                <TreasureList
+                  name={item.name}
+                  country={item.country}
+                  expiration={item.expiration}
+                  id={item.id}
+                  navigation={navigation}
+                  onClickCountry={onClickCountry}
+                  onClickTreasure={onClickTreasure}
+                />
+              );
+            }}
+            keyExtractor={item => item.id}
+          />
+        </View>
       </View>
-    </View>
-  );
+    );
+  }
 }
 
 const styles = StyleSheet.create({
